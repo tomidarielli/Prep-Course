f761a48c5a1acc05c304a067bc03fac4ec4ba131
tomi <tomidarielli@gmail.com>
2022-09-24 18:28:02 -0300

    1save

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,7 +60,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x == y) { return false 
 }
 
 function tienenMismaLongitud(str1, str2) {
