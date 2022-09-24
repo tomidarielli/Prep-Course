8aa40c7fd59435f6bb2bd43e759685f79c0999c6
tomi <tomidarielli@gmail.com>
2022-09-24 18:42:18 -0300

    1save

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -66,21 +66,21 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return str1.lenght == str2.lenght
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90) { return true;} else {return false; }
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {return true;} else {return false;}
 }
 
 function obtenerResto(x, y) {
