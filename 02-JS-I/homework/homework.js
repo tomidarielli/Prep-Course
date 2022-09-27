7a7b4970108013d312ff8fd27c582c9bb47c4d87
tomi <tomidarielli@gmail.com>
2022-09-27 01:40:01 -0300

    3save

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -145,12 +145,8 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  If (numero === 0) ;{return false} if (numero > 0) {return "Es positivo"
-    
-  } else {return "Es negativo"
-    
-  }
-}
+  if(numero === 0) {return false;}
+  else if(numero > 0) {return "Es positivo";} else {return "Es negativo"}}
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
