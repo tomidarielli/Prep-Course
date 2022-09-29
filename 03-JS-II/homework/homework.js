cfb6cb438ef6c8748e7fa2d57f92ae5992d6037f
tomi <tomidarielli@gmail.com>
2022-09-29 02:45:28 -0300

    1save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -88,9 +88,9 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-if (numero / 3 === 0) {return "fizz"}
-else if (numero / 5 === 0) {return "buzz"}
-else if (numero / 3 === 0 && numero / 5 === 0) {return "fizzbuzz"}
+if (numero % 3 === 0) {return "fizz"}
+else if (numero % 5 === 0) {return "buzz"}
+else if (numero % 3 === 0 && numero % 5 === 0) {return "fizzbuzz"}
 else {return numero}
 }
 
