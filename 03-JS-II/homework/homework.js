cb426f66eb6d8f337bf94517879e482719398536
tomi <tomidarielli@gmail.com>
2022-09-29 02:43:13 -0300

    1save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -88,6 +88,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+if (numero / 3 === 0) {return "fizz"}
+else if (numero / 5 === 0) {return "buzz"}
+else if (numero / 3 === 0 && numero / 5 === 0) {return "fizzbuzz"}
+else {return numero}
 }
 
 function operadoresLogicos(num1, num2, num3) {
