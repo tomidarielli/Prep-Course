5f45c0d4622369f92b5252bf712170fc4245ccc9
tomi <tomidarielli@gmail.com>
2022-09-29 02:46:25 -0300

    1save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -90,7 +90,7 @@ function fizzBuzz(numero) {
   // De lo contrario, devuelve el numero
 if (numero % 3 === 0) {return "fizz"}
 else if (numero % 5 === 0) {return "buzz"}
-else if (numero % 3 === 0 && numero % 5 === 0) {return "fizzbuzz"}
+else if (numero % 15 === 0) {return "fizzbuzz"}
 else {return numero}
 }
 
