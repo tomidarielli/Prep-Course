e45162588804fb776d5e91e39a7cbea29517c8b1
tomi <tomidarielli@gmail.com>
2022-09-29 12:56:13 -0300

    2save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -114,10 +114,10 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-if (numero < 2) {return "falso"}
-if (numero === 2) {return true}
-  for (var i = 2; i > numero; i++) {if (numero % i === 1) {return false}
- }
+if (numero < 2) return "falso"
+if (numero === 2) return true
+  for (var i = 2; i < numero; i++) {if (numero % i === 0) {return false}
+ } 
 return true
 }
 
