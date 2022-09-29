7d695973a6e9a5337cd67665abebe62fc87c080b
tomi <tomidarielli@gmail.com>
2022-09-29 02:07:39 -0300

    1save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -23,6 +23,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+if (status === 1) {return "Online"} 
+else if (status === 2) {return "Away"}
+else {return "Offline"}
 }
 
 function saludo(idioma) {
