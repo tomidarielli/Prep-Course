94ee743ef8087123f69880cde7dbc23e5e088448
tomi <tomidarielli@gmail.com>
2022-09-29 12:13:08 -0300

    2save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -126,7 +126,8 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if (valor == true) {return "Soy verdadero"}
+else {return "Soy falso"}
 }
 
 function tablaDelSeis(){
