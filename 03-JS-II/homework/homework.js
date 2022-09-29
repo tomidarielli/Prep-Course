de6c49fca75ae64c8de4da6154c36c53ae7d5da0
tomi <tomidarielli@gmail.com>
2022-09-29 11:51:40 -0300

    2save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -102,8 +102,8 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 if (num1 === 0 || num2 === 0 || num3 ===0) {return "Error"}
-else if (num1 > num2 && num1 > num3 && num1 > 0) {return "Numero 1 es mayor y positivo"}
 else if (num1 < 0 || num2 < 0 || num3 < 0) {return "Hay negativos"}
+else if (num1 > num2 && num1 > num3 && num1 > 0) {return "Numero 1 es mayor y positivo"}
 else if (num3 > num1 && num3 > num2) {return num3 + 1}
 else {return false}
 }
