e236e49b7d101e33374aee7c25a44468a13803a8
tomi <tomidarielli@gmail.com>
2022-09-29 02:04:33 -0300

    1save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,16 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+if (x > y) {return x}
+return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+if (edad >= 18) {return "Allowed"} 
+return "Not allowed";
 }
   
 function conection(status) {
