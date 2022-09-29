5aff2e3f530ef8f7151f7ac8bf5106e1148813e5
tomi <tomidarielli@gmail.com>
2022-09-29 02:11:27 -0300

    1save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -35,6 +35,10 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+if (idioma === "aleman") {return "Guten Tag!"}
+else if (idioma === "mandarin") {return "Ni Hao!"}
+else if (idioma === "ingles") {return "Hello!"}
+else {return "Hola!"}
 }
 
 function colors(color) {
