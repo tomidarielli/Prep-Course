3da701405a79c6bd0b2e187ef070e9c2e7dca058
tomi <tomidarielli@gmail.com>
2022-09-30 02:01:38 -0300

    3save

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -144,7 +144,8 @@ return tabladeseis
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero.length === 3) {return true}
+  return false
 }
 
 function doWhile(numero) {
