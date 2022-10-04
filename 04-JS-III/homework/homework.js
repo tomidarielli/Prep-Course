233209634d5c03907971d9c72ea09e63b60e3869
tomi <tomidarielli@gmail.com>
2022-10-04 12:33:48 -0300

    4save

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -163,7 +163,10 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (var i = 0; i < arreglo.length; i++) {
+    if (arreglo [i] === arreglo [i - 1]) return true
+  }
+  return false;
 } 
 
 
@@ -172,6 +175,13 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var newarray = [];
+for (var i = 0; i < array.length; i++) {
+  if (array [i] === "Enero" || array [i] === "Marzo" || array [i] === "Noviembre")
+  newarray.push (array [i])
+}
+if (newarray.length < 3) return "No se encontraron los meses pedidos";
+else return newarray;
 }
 
 
