382ba2ef88fa6fcf492387ade835d72f97aaced8
tomi <tomidarielli@gmail.com>
2022-10-09 01:52:59 -0300

    2save

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -122,6 +122,12 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+var suma = 0;
+for (var i = 0; i < usuario.posts.length; i++) {
+ suma = suma + usuario ["posts"] [i] ["likes"]
+ 
+}
+return suma;
 }
 
 function agregarMetodoCalculoDescuento (producto) {
