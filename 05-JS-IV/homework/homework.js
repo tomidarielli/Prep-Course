59471a1cd66d052aa0ad8fc3f1f88f49abfda59e
tomi <tomidarielli@gmail.com>
2022-10-09 02:05:38 -0300

    finish

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -132,7 +132,8 @@ return suma;
 
 function agregarMetodoCalculoDescuento (producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
-  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y 
+  // "porcentajeDeDescuento" para obtener el descuento
   // El método resta el descuento del precio y devuelve el precio con descuento
   // Devuelve el objeto "producto" al final de la función
   // Ejemplo:
@@ -140,7 +141,12 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+producto.calcularPrecioDescuento = 
+function () { 
+  return this.precio - (this.precio * this.porcentajeDeDescuento);
+  
+}
+return producto;
 }
 
 // No modificar nada debajo de esta línea
